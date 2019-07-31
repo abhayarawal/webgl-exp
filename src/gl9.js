756f4fcf03ff82669a78d5cf169f49abd0e8a1b7
Abhaya Rawal <abhayarawal@icloud.com>
2019-07-31 02:32:52 -0400

    loop

--- src/gl9.js
+++ src/gl9.js
@@ -1,17 +1,24 @@
 import { animationFrameScheduler, interval, of } from 'rxjs';
+import { withLatestFrom, take } from 'rxjs/operators';
 
 var sceneGraph = {
   entities: {
     meshes: {
     }
   }
-}
+};
 
+let state$ = of(sceneGraph);
 interval(0, animationFrameScheduler)
 .pipe(
+  withLatestFrom(state$),
+  take(10)
 )
-.subscribe(_ => {
+.subscribe(([_, { entities }]) => {
 
-})
+  let { meshes } = entities;
+  console.log(meshes)
+
+});
 
 // mesh - buffer[attrs]; 
