2fd8d69acf7714383cbc2e8d5e10201675f49c1f
Abhaya Rawal <avay_raul@hotmail.com>
2019-07-31 11:57:25 -0400

    mat4

--- src/gl9.js
+++ src/gl9.js
@@ -1,5 +1,6 @@
 import { animationFrameScheduler, interval, of, BehaviorSubject } from 'rxjs';
 import { withLatestFrom, take, distinctUntilChanged, delay, map } from 'rxjs/operators';
+import { mat4, quat2 } from 'gl-matrix';
 import { createProgramWithShaders } from './utility/common';
 import { StoreFactory } from './Store';
 
@@ -21,20 +22,43 @@ var sceneGraphInitial = {
 var sceneGraph = StoreFactory(sceneGraphInitial);
 
 
-var createMesh = (model) => {
+var createMesh = (model, translation = [0, 0, 0], rotation = [0, 0, 0]) => {
   let state = sceneGraph.snapshot(),
       { gl } = state,
       ref = (+new Date()).toString(16) + '.' + (Math.random() * 10000000 | 0).toString(16),
-      { positions, elements, vertexNormals }  = model,
-      mesh = { 
-        [ref]: {
-          render: true,
-          vertexNormals,
-          buffers: {
-            // vertexPositionBuffer: 
-          }
-        }
-      };
+      { positions, elements, vertexNormals }  = model;
+      
+  let vertexPositionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
+
+  let idxBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, idxBuffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, elements, gl.STATIC_DRAW);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+  let modelMat4 = mat4.create(),
+      q = quat2.create(),
+      [x, y, z] = rotation;
+  
+  quat2.rotateX(q, q, x);
+  quat2.rotateY(q, q, x);
+  quat2.rotateZ(q, q, x);
+  mat4.fromRotationTranslation(modelMat4, q, translation);
+
+  let mesh = { 
+    [ref]: {
+      render: true,
+      vertexNormals,
+      buffers: {
+        vertexPositionBuffer,
+        idxBuffer
+      },
+      modelMat4
+    }
+  };
 
   const meshes = { ...state.entities.meshes, ...mesh }
   
@@ -42,6 +66,14 @@ var createMesh = (model) => {
 }
 
 
+
+
+
+// createMesh(bunnyModel)
+
+var updateMeshes = () => {
+
+}
 
 let entities$ = sceneGraph.select('entities')
 
