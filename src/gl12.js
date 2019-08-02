320e03587a79304878e1ed13f428f02b74953294
Abhaya Rawal <abhayarawal@icloud.com>
2019-08-02 17:17:17 -0400

    ref

--- src/gl12.js
+++ src/gl12.js
@@ -6,28 +6,27 @@
 
   var parseGltf = (raw) => {
     let scene = {
+      meshes: {}
     }
 
     let nodes = raw.nodes,
-        meshes = [],
         traverseNode = (n) => {
           let node = nodes[n];
           ('mesh' in node) ? 
-            meshes.push(node) :
+            scene.meshes[newRef()] = node :
             ('children' in node) ? 
               node.children.forEach(traverseNode) : 
               null;
         };
 
     traverseNode(raw.scenes[0].nodes[0]);
-    scene.meshes = meshes;
 
     console.log(scene);
   };
 
 
-  fetch(`https://akute.nyc3.digitaloceanspaces.com/engine/cube/cube.gltf.json`)
-  // fetch(`https://akute.nyc3.digitaloceanspaces.com/engine/door/scene.gltf.json`)
+  // fetch(`https://akute.nyc3.digitaloceanspaces.com/engine/cube/cube.gltf.json`)
+  fetch(`https://akute.nyc3.digitaloceanspaces.com/engine/door/scene.gltf.json`)
   .then(response => {
     response.json().then(data => {
       parseGltf(data);
