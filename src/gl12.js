d33be1717fc83015f2eb1a33f12c63ee62fe4660
Abhaya Rawal <abhayarawal@icloud.com>
2019-08-02 17:41:54 -0400

    ref

--- src/gl12.js
+++ src/gl12.js
@@ -1,19 +1,19 @@
 (function () {
+  let relRoute = `https://akute.nyc3.digitaloceanspaces.com/engine/cube/`;
 
   var newRef = () => {
     return (+new Date()).toString(16) + '.' + (Math.random() * 10000000 | 0).toString(16);
   }
 
   var parseGltf = (raw) => {
-    let scene = {
-      meshes: {}
-    }
+    let scene = {},
+        meshes = {};
 
     let nodes = raw.nodes,
         traverseNode = (n) => {
           let node = nodes[n];
           ('mesh' in node) ? 
-            scene.meshes[newRef()] = node :
+            meshes[newRef()] = raw.meshes[node.mesh] :
             ('children' in node) ? 
               node.children.forEach(traverseNode) : 
               null;
@@ -21,12 +21,15 @@
 
     traverseNode(raw.scenes[0].nodes[0]);
 
-    console.log(scene);
+    Object.keys(meshes).forEach(meshRef => {
+      let mesh = meshes[meshRef];
+      console.log(mesh);
+    })
   };
 
 
-  // fetch(`https://akute.nyc3.digitaloceanspaces.com/engine/cube/cube.gltf.json`)
-  fetch(`https://akute.nyc3.digitaloceanspaces.com/engine/door/scene.gltf.json`)
+  fetch(`${relRoute}cube.gltf.json`)
+  // fetch(`https://akute.nyc3.digitaloceanspaces.com/engine/door/scene.gltf.json`)
   .then(response => {
     response.json().then(data => {
       parseGltf(data);
